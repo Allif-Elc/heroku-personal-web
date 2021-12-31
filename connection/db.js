46f43c77a465d76296292dad6ae730429ba4d72c
Allif=Elc <allif.taufiq13@gmail.com>
2021-12-31 11:23:22 +0700

    heroku config 1.1

--- connection/db.js
+++ connection/db.js
@@ -1,17 +1,17 @@
 const { Pool } = require('pg');
 
 const dbPool = new Pool({
-    // database: 'personal_web',
-    // port: 5432,
-    // user: 'postgres',
-    // password: 'root' 
+//    database: 'personal_web',
+//    port: 5432,
+//    user: 'postgres',
+//    password: 'root' 
 
-connectionString:
-    'postgres://bayryxrtjdffax:2c4c79a03e645f4cbb2d74e9acd597de8e27949f5c2abc03abf92d52e2135b75@ec2-52-70-205-234.compute-1.amazonaws.com:5432/dce79o9e19k7dn',
-ssl: {
+ connectionString:
+ 'postgres://bayryxrtjdffax:2c4c79a03e645f4cbb2d74e9acd597de8e27949f5c2abc03abf92d52e2135b75@ec2-52-70-205-234.compute-1.amazonaws.com:5432/dce79o9e19k7dn',
+ ssl: {
     rejectUnauthorized: false,
 }
-});
+});h
 
 
 module.exports = dbPool;
